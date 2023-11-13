5677a0f4e20b20b5a7fcbdbe414cc22af81acac3
DYung26 <theadstore1407@gmail.com>
2023-11-13 12:31:15 -0800

    Add Bash script for Git operations

--- lazy.sh
+++ lazy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-echo "Enter file name!:"
+
+echo "Enter file name:"
 read file_name
-git add ${file_name}
+
+git add "${file_name}"
+
 echo "Enter Your Commit Message:"
 read commit_msg
+
 git commit -m "${commit_msg}"
+
 echo -e "\n***Message Read Sucessfully***\n"
+
 git push
