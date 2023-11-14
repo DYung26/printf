a6d2a60468c556889ade8e1d8a4fe798d58c604b
DYung26 <theadstore1407@gmail.com>
2023-11-14 02:27:19 -0800

    Added main header file 'main.h' with the necessary declarations for the printf function, including flags, sizes, and function prototypes for handling various specifiers.

--- main.h
+++ main.h
@@ -1,37 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
+#include <stdio.h>
 #include <unistd.h>
-
-typedef struct format
-{
-	char *id;
-	int (*f)();
-}match;
-
-int printf_rot13(va_list val);
-int printf_reverse_string(va_list val);
-int printf_pointer(va_list val);
-int printf_hex_extra(unsigned long int num);
-int printf_string(va_list val);
-int printf_HEX_extra(unsigned int num);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_binary(va_list val);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int printf_37(void);
-int _strlen(const char *str);
-int printf_s(va_list val);
-int printf_char(va_list val);
-
-#endif
 
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
@@ -68,8 +39,6 @@ struct fmt
  */
 typedef struct fmt fmt_t;
 
-int printf_str1(va_list val);
-int _putchar(char c);
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
@@ -145,4 +114,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif
+#endif /* MAIN_H */
