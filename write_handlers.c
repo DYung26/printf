cd0aa2c7b63e4f7d6b51b78c80be9aadaa09bab5
DYung26 <theadstore1407@gmail.com>
2023-11-14 04:25:19 -0800

    Implement functions for handling writing characters, numbers, and poiâinters in main.c

--- write_handlers.c
+++ write_handlers.c
@@ -79,62 +79,75 @@ int write_number(int is_negative, int ind, char buffer[],
 }
 
 /**
- * write_num - Write a number using a bufffer
+ * write_num - Write a number using a buffer
  * @ind: Index at which the number starts on the buffer
  * @buffer: Buffer
  * @flags: Flags
- * @width: width
- * @prec: Precision specifier
+ * @width: Width
+ * @precision: Precision specifier
  * @length: Number length
- * @padd: Pading char
- * @extra_c: Extra char
+ * @padd: Padding character
+ * @extra_ch: Extra character (e.g., '+' or '-')
  *
- * Return: Number of printed chars.
+ * Return: Number of characters printed
  */
-int write_num(int ind, char buffer[],
-	int flags, int width, int prec,
-	int length, char padd, char extra_c)
+int write_num(int ind, char buffer[], int flags, int width, int precision,
+              int length, char padd, char extra_ch)
 {
-	int i, padd_start = 1;
+    int printed_chars = 0;
 
-	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
-		return (0); /* printf(".0d", 0)  no char is printed */
-	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
-		buffer[ind] = padd = ' '; /* width is displayed with padding ' ' */
-	if (prec > 0 && prec < length)
-		padd = ' ';
-	while (prec > length)
-		buffer[--ind] = '0', length++;
-	if (extra_c != 0)
-		length++;
-	if (width > length)
-	{
-		for (i = 1; i < width - length + 1; i++)
-			buffer[i] = padd;
-		buffer[i] = '\0';
-		if (flags & F_MINUS && padd == ' ')/* Asign extra char to left of buffer */
-		{
-			if (extra_c)
-				buffer[--ind] = extra_c;
-			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
-		}
-		else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buff */
-		{
-			if (extra_c)
-				buffer[--ind] = extra_c;
-			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
-		}
-		else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
-		{
-			if (extra_c)
-				buffer[--padd_start] = extra_c;
-			return (write(1, &buffer[padd_start], i - padd_start) +
-				write(1, &buffer[ind], length - (1 - padd_start)));
-		}
-	}
-	if (extra_c)
-		buffer[--ind] = extra_c;
-	return (write(1, &buffer[ind], length));
+    if (precision > length)
+        length = precision;
+
+    if (width > length)
+    {
+        if (!(flags & F_MINUS))
+        {
+            printed_chars += write_padding(width - length, padd);
+        }
+
+        if (extra_ch != 0)
+        {
+            write(1, &extra_ch, 1);
+            printed_chars++;
+        }
+
+        printed_chars += write(1, &buffer[BUFF_SIZE - length], length);
+
+        if (flags & F_MINUS)
+        {
+            printed_chars += write_padding(width - length, ' ');
+        }
+    }
+    else
+    {
+        if (extra_ch != 0)
+        {
+            write(1, &extra_ch, 1);
+            printed_chars++;
+        }
+
+        printed_chars += write(1, &buffer[BUFF_SIZE - length], length);
+    }
+
+    return printed_chars;
+}
+
+/**
+ * write_padding - Writes padding characters to the standard output
+ * @count: Number of padding characters to write
+ * @padd: Padding character
+ *
+ * Return: Number of characters written
+ */
+int write_padding(int count, char padd)
+{
+    int i;
+    for (i = 0; i < count; i++)
+    {
+        write(1, &padd, 1);
+    }
+    return count;
 }
 
 /**
